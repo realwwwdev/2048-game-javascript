554861e8a0a89d11fb6c0e5e077dc89d080e206f
real wwwdev <sheen3d@yandex.ru>
2023-04-30 12:09:47 +0500

    creating script.js & adding some code

---
+++ script.js
@@ -0,0 +1,7 @@
+import { Grid } from './grid.js';
+import { Tile } from './tile.js';
+
+const gameBoard = document.querySelector('#game-board');
+
+const grid = new Grid(gameBoard);
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
